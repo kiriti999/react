34cd5e619fe31d7a4bb54c0effa15fb0d8fd3e44
kiriti <kiriti.k999@gmail.com>
2022-12-26 23:03:57 +0530

    up

--- react-projects/06-tabs/setup/src/App.js
+++ react-projects/06-tabs/setup/src/App.js
@@ -4,7 +4,62 @@ import { FaAngleDoubleRight } from 'react-icons/fa'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
-  return <h2>tabs project setup</h2>
+  const [loading, setloading] = useState(true);
+  const [jobs, setjobs] = useState([]);
+  const [value, setvalue] = useState(0);
+
+  const fetchJobs = async () => {
+    const res = await fetch(url);
+    const newJobs = await res.json();
+    setjobs(newJobs);
+    setloading(false);
+  }
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>loading...</h1>
+      </section>
+    )
+  }
+
+  const { company, dates, duties, title } = jobs[value];
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        <div className="btn-container">
+          {
+            jobs.map((item, i) => {
+              return <button key={i} onClick={() => setvalue(i)} className={`job-btn ${i === value && 'active-btn'}`}>{item.company}</button>
+            })
+          }
+        </div>
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, index) => {
+            return <div key={index} className='job-desc'>
+              <FaAngleDoubleRight className='job-icon'></FaAngleDoubleRight>
+              <p>{duty}</p>
+            </div>
+          })}
+        </article>
+      </div>
+    </section>
+  )
+
+
+
 }
 
 export default App
