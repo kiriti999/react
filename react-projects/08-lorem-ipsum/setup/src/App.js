8c2613d57ef1800465585c917bafa7a1ca478672
kiriti <kiriti.k999@gmail.com>
2023-01-02 19:36:09 +0530

    colors

--- react-projects/08-lorem-ipsum/setup/src/App.js
+++ react-projects/08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,40 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
-  
-  
+  let [count, setcount] = useState(0);
+  const [text, settext] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (count <= 0) {
+      count = 1
+    }
+    if (count > 8) {
+      count = 8
+    }
+    settext(data.slice(0, parseInt(count)));
+  }
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className='section-center'>
+      <h3>Lorem ipsum</h3>
+      <form onSubmit={handleSubmit} className="lorem-form">
+        <label htmlFor="amount" id=''>Paragraphs:</label>
+        <input type="number" name="amount" value={count} onChange={(e) => setcount(e.target.value)} />
+        <button type="submit" className="btn">generate</button>
+      </form>
+
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return (
+            <div key={index}>
+              <p>{item}</p>
+            </div>
+          )
+        })}
+      </article>
+    </section>
+  )
 }
 
 export default App;
--- react-projects/09-color-generator/setup/src/App.js
+++ react-projects/09-color-generator/setup/src/App.js
@@ -4,7 +4,40 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setcolor] = useState('')
+  const [error, seterror] = useState(false)
+  const [list, setlist] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      setlist(colors);
+      seterror(false);
+      console.log('file: App.js:15 :: colors', colors);
+    } catch (err) {
+      seterror(true);
+      console.log('file: App.js:17 :: err', err)
+    }
+  }
+
+  return (
+    <div>
+      <section className="container">
+        <h3>color generator</h3>
+        <form action="" onSubmit={handleSubmit}>
+          <input type="text" value={color} onChange={(e) => setcolor(e.target.value)} placeholder='#'
+            className={`${error ? 'error' : null}`} />
+          <button className="btn" type="submit">Submit</button>
+        </form>
+      </section>
+      <section className="colors">
+        {list.map((color, index) => {
+          return <SingleColor key={index} {...color} index={index}></SingleColor>
+        })}
+      </section>
+    </div>
+  )
 }
 
 export default App
