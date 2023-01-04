ad21430e08a4f6c4666d8ad37e70ce596c6830ac
kiriti <kiriti.k999@gmail.com>
2023-01-04 21:43:41 +0530

    use ref

--- react-advanced-2020/src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ react-advanced-2020/src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -5,7 +5,24 @@ import React, { useEffect, useRef } from 'react';
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+  const refContainer = useRef(null)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('file: 1-useRef-basics.js:9 :: refContainer', refContainer.current.value)
+  }
+
+  useEffect(() => refContainer.current.focus());
+
+  return (
+    <div>
+      <form action="" onSubmit={handleSubmit} className="form">
+        <div>
+          <input type="text" ref={refContainer} />
+          <button className="btn">Submit</button>
+        </div>
+      </form>
+    </div>
+  )
 };
 
 export default UseRefBasics;
