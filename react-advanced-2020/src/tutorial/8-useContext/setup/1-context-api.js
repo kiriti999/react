d3e02232b7097cc4c599f09f2e738cead0bf5c66
kiriti <kiriti.k999@gmail.com>
2023-01-10 00:33:19 +0530

    context api

--- react-advanced-2020/src/tutorial/8-useContext/setup/1-context-api.js
+++ react-advanced-2020/src/tutorial/8-useContext/setup/1-context-api.js
@@ -3,6 +3,9 @@ import { data } from '../../../data';
 // more components
 // fix - context api, redux (for more complex cases)
 
+const PersonContext = React.createContext();
+
+
 const ContextAPI = () => {
   const [people, setPeople] = useState(data);
   const removePerson = (id) => {
@@ -13,12 +16,15 @@ const ContextAPI = () => {
   return (
     <>
       <h3>prop drilling</h3>
-      <List people={people} removePerson={removePerson} />
+      <PersonContext.Provider value={{ removePerson, people }}>
+        <List />
+      </PersonContext.Provider>
     </>
   );
 };
 
-const List = ({ people, removePerson }) => {
+const List = () => {
+  const { people } = useContext(PersonContext)
   return (
     <>
       {people.map((person) => {
@@ -26,7 +32,6 @@ const List = ({ people, removePerson }) => {
           <SinglePerson
             key={person.id}
             {...person}
-            removePerson={removePerson}
           />
         );
       })}
@@ -34,7 +39,8 @@ const List = ({ people, removePerson }) => {
   );
 };
 
-const SinglePerson = ({ id, name, removePerson }) => {
+const SinglePerson = ({ id, name }) => {
+  const { removePerson } = useContext(PersonContext)
   return (
     <div className='item'>
       <h4>{name}</h4>
