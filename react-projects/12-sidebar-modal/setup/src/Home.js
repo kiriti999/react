3081f505631d369318a694a72d40b4863d760334
kiriti <kiriti.k999@gmail.com>
2023-02-07 11:37:05 +0530

    up

--- react-projects/12-sidebar-modal/setup/src/Home.js
+++ react-projects/12-sidebar-modal/setup/src/Home.js
@@ -1,13 +1,15 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import { FaBars } from 'react-icons/fa'
+import { useGlobal } from './context';
 
 const Home = () => {
+  const { openSidebar, openModal } = useGlobal();
   return (
     <main>
-      <button className="sidebar-toggle">
+      <button className="sidebar-toggle" onClick={openSidebar}>
         <FaBars></FaBars>
       </button>
-      <button className="btn">Show modal</button>
+      <button className="btn" onClick={openModal}>Show modal</button>
     </main>
   )
 }
--- react-projects/12-sidebar-modal/setup/src/context.js
+++ react-projects/12-sidebar-modal/setup/src/context.js
@@ -1,5 +1,41 @@
 import React, { useState, useContext } from 'react'
 
-const AppProvider = (second) => { }
+const AppContext = React.createContext();
 
-export { AppProvider };
+const AppProvider = ({ children }) => {
+    const [isSidebarOpen, setisSidebarOpen] = useState(false);
+    const [isModalOpen, setisModalOpen] = useState(false);
+
+
+    const openSidebar = () => {
+        setisSidebarOpen(true);
+    }
+
+    const closeSidebar = () => {
+        setisSidebarOpen(false);
+    }
+
+    const openModal = () => {
+        setisModalOpen(true)
+    }
+
+    const closeModal = () => {
+        setisModalOpen(false)
+    }
+
+    return <AppContext.Provider value={{
+        isModalOpen,
+        isSidebarOpen,
+        openModal,
+        openSidebar,
+        closeModal,
+        closeSidebar
+    }}>{children}</AppContext.Provider>
+}
+
+// custom hook
+export const useGlobal = () => {
+    return useContext(AppContext)
+}
+
+export { AppContext, AppProvider };
